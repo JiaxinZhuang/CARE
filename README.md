2d2f55e677385f537f4d69455e23443d1162330d
Jiaxin Zhuang <lincolnz9511@gmail.com>
2019-12-06 16:59:31 +0800

    add loss and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gc_fs
+## Pytorch Implementatoin for: CARE: Class Attention to Regions of Lesion for Classification on Imbalanced Data. (MIDL-2019)
+***
+
+We would continue updating all the codes in the near future.
+This repo would contain the pytorch implemetation for CARE. 
+
+### Requiements
+* Python 3.6+
+* Pytorch 1.3
+* Cuda 9.2+ or Cuda 10.1+
--- src/loss.py
+++ src/loss.py
@@ -5,14 +5,14 @@ import sys
 import torch
 from torch import nn
 
+
 class GradLoss(nn.Module):
     """GradLoss
     """
 
-    def __init__(self, inner_threshold, logger):
+    def __init__(self, inner_threshold):
         super(GradLoss, self).__init__()
         self.inner_threshold = torch.tensor(inner_threshold).cuda()
-        self.logger = logger
 
     def forward(self, grad_batch, inner_batch, outer_batch):
         return self.grad_loss(grad_batch, inner_batch, outer_batch)
@@ -30,25 +30,44 @@ class GradLoss(nn.Module):
 
         inner_loss = (grad * inner).sum()
         if torch.isnan(inner_loss):
-            self.logger.error('grad {}'.format(grad.size()))
-            self.logger.error('grad {}'.format(grad))
-            self.logger.error('inner {}'.format(inner.size()))
-            self.logger.error('inner {}'.format(inner))
-            self.logger.error('inner_loss: {}'.format(inner_loss))
+            print(grad.size())
+            print(grad)
+            print(inner.size())
+            print(inner)
+            print(inner_loss)
             sys.exit(-1)
         if inner_loss > self.inner_threshold:
-            #inner_loss = torch.Tensor(inner_threshold).squeeze()
             inner_loss = self.inner_threshold.squeeze()
         outer_loss = (grad * outer).sum()
         grad_loss = -inner_loss + outer_loss
         return grad_loss
 
-    #loss_sum = 0
-    #for grad, inner, outer in zip(grad_batch, inner_batch, outer_batch):
-    #    inner_loss = (grad * inner).sum()
-    #    if inner_loss > inner_threshold:
-    #        inner_loss = torch.Tensor(inner_threshold).squeeze()
-    #    outer_loss = (grad * outer).sum()
-    #    loss = -inner_loss + outer_loss
-    #    loss_sum = loss_sum + loss
-    #return loss_sum
+
+def grad(target_activation, grad_val):
+    """Grad for each image.
+    Args:
+        target_activation: [2048, 7, 7]
+        output: [7]
+        grad_val: [2048, 7, 7]
+    Return:
+        grad: [224,224]
+    """
+
+    # [2048, 7, 7]
+    channels, height, width = grad_val.size()
+    # [2048, 49]
+    grad_val = grad_val.view(channels, -1)
+    weights = torch.mean(grad_val, dim=1)
+    # [2048]
+    weights = weights.view(channels, 1, 1)
+    cam = weights * target_activation
+
+    # [2048]
+    cam = torch.sum(cam, dim=0)
+    cam = cam - torch.min(cam)
+    cam = cam / torch.max(cam)
+    cam = cam.view(1, 1, height, width)
+    cam = torch.nn.functional.interpolate(cam, size=(224, 224),
+                                          mode='bilinear')
+    cam = cam.view(224, 224)
+    return cam
